c4e2d0d2a285bd97269994c617713228af9534de
Patrick McMahon <pmcmahon14@gmail.com>
2019-10-08 15:37:29 -0700

    stats row styled

--- components/Statistics.js
+++ components/Statistics.js
@@ -4,12 +4,12 @@ import {View, Text, StyleSheet} from 'react-native';
 const Statistics = props => {
     return (
     <View style={styles.statBar}>
-        <Text style={styles.stat}>Menu</Text>
-        <Text style={styles.stat}>Attempts</Text>
-        <Text style={styles.stat}>Matches</Text>
-        <Text style={styles.stat}>Accuracy</Text>
-        <Text style={styles.stat}>Wins</Text>
-        <Text style={styles.stat}>Reset</Text>
+        <Text style={styles.menu}>Menu</Text>
+        <Text style={styles.stat}>Attempts{"\n"}0</Text>
+        <Text style={styles.stat}>Matches{"\n"}0</Text>
+        <Text style={styles.stat}>Accuracy{"\n"}0</Text>
+        <Text style={styles.stat}>Wins{"\n"}0</Text>
+        <Text style={styles.reset}>Reset</Text>
     </View>
     )
 };
@@ -18,13 +18,24 @@ const styles = StyleSheet.create({
     statBar: {
         flexDirection: 'row',
         marginTop: 30,
-        height: 40,
-        backgroundColor: 'gray',
-        alignItems: 'center',
-        justifyContent: 'center'
+        height: 40
+    },
+    menu: {
+        width: '16%',
+        backgroundColor: 'lightgray',
+        textAlign: 'center'
     },
     stat: {
+        width: '15%',
+        backgroundColor: 'gray',
+        marginLeft: '1%',
+        textAlign: 'center'
+    },
+    reset: {
         width: '16%',
+        backgroundColor: 'red',
+        marginLeft: '1%',
+        textAlign: 'center'
     }
 });
 
